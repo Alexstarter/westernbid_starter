09c3e3e42ac458de0c7394d0fa8b5b066e442f12
root <root@core.starter.design>
2024-05-08 18:33:15 +0300

    first commit

---
+++ controllers/front/callback.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+/**
+ * This Controller simulate an external payment gateway
+ */
+class Westernbid_StarterCallbackModuleFrontController extends ModuleFrontController
+{
+    public function postProcess()
+    {
+        $output = Tools::file_get_contents("php://input");
+        $data = array();
+        parse_str($output, $data);
+
+        $secret_key = Configuration::get(Westernbid_Starter::STARTER_WB_SECRETKEY);
+        $wb_login = Configuration::get(Westernbid_Starter::STARTER_WB_LOGIN);
+        $wb_result = $data['wb_result'];
+        $mc_gross = $data['mc_gross'];
+        $invoice = $data['invoice'];
+
+        $wb_hash = md5($wb_login.$wb_result.$secret_key.$mc_gross.$invoice);
+
+        if ($wb_hash == $data['wb_hash'])
+        {
+            $id_order = (int) Tools::getValue('id_order');
+            // Completed
+            if ($data['payment_status'] == 'Pending')
+            {
+                $orderHistory = new OrderHistory();
+                $orderHistory->id_order = $id_order;
+                $orderHistory->changeIdOrderState(
+                    Configuration::get('PS_OS_PAYMENT'),
+                    $id_order
+                );
+
+                $orderHistory->addWithemail(); 
+            }
+        }
+        die();
+    }
+}
